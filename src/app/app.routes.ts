f5a9a1ef0efb9ce42e48fc5ddf33cad4b9e3c4d7
pmsantanna <pedro1997santanna@gmail.com>
2025-05-19 09:52:51 -0300

    add landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,10 @@ import { HomeComponent } from './pages/home/home.component';
 import { ProdutosComponent } from './pages/produtos/produtos.component';
 import { ContatoComponent } from './pages/contato/contato.component';
 import { SobreComponent } from './pages/sobre/sobre.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: LandingComponent },
   { path: 'produtos', component: ProdutosComponent },
   { path: 'contato', component: ContatoComponent },
   { path: 'sobre', component: SobreComponent },
